05e8cbb49d0bf00bf25e3903ada197855b8bc539
Fernando Paredes Rios <fprios112@gmail.com>
2019-09-30 18:23:52 -0500

    primer commit 2

---
+++ Java/Car.java
@@ -0,0 +1,12 @@
+class Car{
+    Integer id;
+    String license;
+    String driver;
+    Integer passenger;
+
+
+    void printDataCar(){
+        System.out.println("License: " + license + " Driver: " + driver);
+    }
+
+}
---
+++ PHP/Route.php
@@ -0,0 +1,6 @@
+<?php
+class Rout{
+    public $id;
+    public $start = array();
+    public $end = array();
+}
