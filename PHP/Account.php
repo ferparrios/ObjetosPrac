7b0b2bd2cd1f54ea7aa8da1be2c2b0ddedf59fdb
Fernando Paredes Rios <fprios112@gmail.com>
2019-10-01 23:30:23 -0500

    segundo avance del curso

--- PHP/Account.php
+++ PHP/Account.php
@@ -7,4 +7,12 @@ class Account
     public $document;
     public $email;
     public $password;
+
+
+    //metodo contructor
+    public function __construct($name, $document){
+        $this->name = $name;
+        $this->document = $document;
+    }
+
 }
--- Python/main.py
+++ Python/main.py
@@ -1,2 +1,14 @@
+from car import Car
+
 if __name__== "__main__":
-    print("Hello World whit Fer")
+    print("Hola Fer")
+    car = Car()
+    car.license = "AMS234"
+    car.driver = "Andres Herrera"
+    print(vars(car))
+
+    car2 = Car()
+    car2.license = "ABC123"
+    car2.driver = "Andrea Herrera"
+    print(vars(car2))
+
